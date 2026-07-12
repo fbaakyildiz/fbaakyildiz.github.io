c309b0680bb68aadf2bfb02aed86409722649b72
Fatih Berker Akyildiz <fbaakyildiz@gmail.com>
2026-07-12 17:44:04 +0200

    update

--- script.js
+++ script.js
@@ -188,7 +188,7 @@ function animateProjectCount(value) {
     targets: countState,
     value,
     round: 1,
-    delay: 1000,
+    delay: 700,
     duration: 1400,
     easing: "easeOutCubic",
     update: () => {
